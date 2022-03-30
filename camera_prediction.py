febb9fa9c1327bec81599144168eb73c66b7d1d9
AlesVysocky <AlesVysocky@users.noreply.github.com>
2022-03-30 15:14:47 +0200

    camera D435 HF_Filter

--- camera_prediction.py
+++ camera_prediction.py
@@ -1,12 +1,14 @@
 from mmdet.apis import init_detector, show_result_ins, predict_image
 import time
-import pyrealsense2 as rs
+
 import numpy as np
 import cv2
-
+import sys
+sys.path.insert(0,r"C:\Users\C201_ALES\source\repos\hand_tracker\HGR_CNN")
+import pyrealsense2 as rs
 
 config_file = '../SOLO/configs/solov2/solov2_r101_fpn_custom.py'
-checkpoint_file = '../SOLO/checkpoints/epoch_2.pth'
+checkpoint_file = '../SOLO/checkpoints/r101_e7_two_hands.pth'
 
 # config_file = '../SOLO/configs/solov2/solov2_light_448_r50_fpn_custom.py'
 # checkpoint_file = '../SOLO/checkpoints/s2ch4_epoch_10.pth'
@@ -41,7 +43,19 @@ pipeline_config.enable_stream(rs.stream.color, img_camera_size[0], img_camera_si
 profile = pipeline.start(pipeline_config)
 depth_sensor = profile.get_device().first_depth_sensor()
 depth_scale = depth_sensor.get_depth_scale()
+
+#D515
 # depth_sensor.set_option(rs.option.visual_preset,5)
+
+#D435
+filter_HF = rs.hole_filling_filter()
+filter_HF.set_option(rs.option.holes_fill, 3)
+colorizer = rs.colorizer()
+colorizer.set_option(rs.option.visual_preset,1)
+colorizer.set_option(rs.option.min_distance,0.2)        
+colorizer.set_option(rs.option.max_distance,1.05)
+colorizer.set_option(rs.option.color_scheme,2)
+colorizer.set_option(rs.option.histogram_equalization_enabled,0)
 
 align_to = rs.stream.color
 align = rs.align(align_to)
@@ -51,10 +65,17 @@ while(True):
     aligned_frames = align.process(frames) # aligned frames
     depth_frame = aligned_frames.get_depth_frame()
     color_frame = aligned_frames.get_color_frame()
-
+    
+    #D435
+    filtered = filter_HF.process(depth_frame)
+    filtered = colorizer.colorize(filtered)
+    depth_colorized = np.asanyarray(filtered.get_data())
+    depth_colorized = depth_colorized[:,:,0:1]
+    #depth_colorized = depth_colorized[..., np.newaxis]
+    
+    #D515
     depth_image = np.asanyarray(depth_frame.get_data())
     color_image = np.asanyarray(color_frame.get_data())
-
     rawdepth = np.asanyarray(depth_frame.get_data())
     min = 0.2
     max = 1.05
@@ -62,7 +83,12 @@ while(True):
     norm = lambda n: n/max
     mapped_depth = 255-norm(mapped_depth)*255
     mapped_depth = mapped_depth[..., np.newaxis]
-    rgbd = np.concatenate([color_image,mapped_depth.astype("uint8")],axis=2)
+
+    #D515
+    #rgbd = np.concatenate([color_image,mapped_depth.astype("uint8")],axis=2)
+
+    #D435
+    rgbd = np.concatenate([color_image,depth_colorized.astype("uint8")],axis=2)
     detect(rgbd)
     
 pipeline.stop()
