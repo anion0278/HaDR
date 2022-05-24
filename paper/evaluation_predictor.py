305b058739a3e609554c933d7285f776823b9dbd
AlesVysocky <AlesVysocky@users.noreply.github.com>
2022-05-24 08:53:11 +0200

    unified threshold

--- paper/evaluation_predictor.py
+++ paper/evaluation_predictor.py
@@ -9,10 +9,10 @@ import common_settings as s
 s.add_packages_paths()
 from mmdet.apis import init_detector, show_result_ins, predict_image, show_result_pyplot
 
-image_dir = r"C:\datasets\real_merged_l515_640x480"
+image_dir = r"E:\datasets\real_merged_l515_640x480"
 # checkpoint_dir = "2G-solov2_light_448_r50_fpn_4ch-sim_train_320x256_full-AugTrue-10+20ep"
-checkpoint_dir = "2R-solov2_r101_fpn_3ch-real_merged_l515_640x480_full-AugTrue-10+20ep-Wed_D18_M05_10h_14m"
-
+checkpoint_dir = "2T-solov2_r101_fpn_4ch-sim_train_320x256_full-AugTrue-30+20ep-Wed_D18_M05_13h_25m"
+threshold = 0.5
 
 def get_tested_image(input_channels, img_bgrd):
     options = { 1: img_bgrd[:,:,3:4], 
@@ -23,14 +23,15 @@ def get_tested_image(input_channels, img_bgrd):
 def detect(img_bgrd, arch):
     start = time.time()
     tested_image = get_tested_image(cfg.model.backbone.in_channels, img_bgrd)
+    #tested_image = cv2.resize(tested_image,[320,256])
     result = predict_image(model, tested_image)
     elapsed_time = time.time() - start
     title = f"Inference time: {elapsed_time:2.2f}s, FPS: {(1/elapsed_time):2.0f}" 
     color, depth = image_loader.separate_color_from_depth(img_bgrd)
     depth = np.stack((np.squeeze(depth),)*3, axis=-1)
     ins_visualization = show_result_pyplot if "mask" in arch else show_result_ins # mask rcnn requires different visualization
-    res_img_bgrb = ins_visualization(color, result, model.CLASSES, score_thr=0.10)
-    res_img_d = ins_visualization(depth, result, model.CLASSES, score_thr=0.10)
+    res_img_bgrb = ins_visualization(color, result, model.CLASSES, score_thr=threshold)
+    res_img_d = ins_visualization(depth, result, model.CLASSES, score_thr=threshold)
     window_id = "win id"
     cv2.imshow(window_id, np.hstack([res_img_bgrb, res_img_d]))
     cv2.setWindowTitle(window_id, title)
@@ -43,8 +44,8 @@ if __name__ == "__main__":
     cfg = utils.get_config(arch, channels)
     model = init_detector(cfg, checkpoint_path + "/" + s.tested_checkpoint_file_name, device='cuda:0')
     if len(model.CLASSES) > 1: 
-        print(f"Overrinding the model classes! Current classes: {model.CLASSES}")
-        model.CLASSES = ["person"] if cfg.model.bbox_head.num_classes == 81 else ["hand"]
+         print(f"Overrinding the model classes! Current classes: {model.CLASSES}")
+         model.CLASSES = ["person"] if cfg.model.bbox_head.num_classes == 81 else ["hand"]
             
     loader = image_loader.ImageLoader(image_dir)
     while(True):
