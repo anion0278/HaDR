60211a7c5602153766cfe6385d4db9bffc8d0cb6
anion0278 <anion0278@gmail.com>
2022-05-31 10:18:27 +0200

    any folders are now allowed for selection

--- paper/tensorboard_starter.py
+++ paper/tensorboard_starter.py
@@ -4,6 +4,17 @@ import webbrowser
 import common_settings as s
 import model_utils as utils
 import ws_specific_settings as wss
+import tkinter as tk
+from tkinter import filedialog
+
+def ask_user_for_log_dir(default_checkpoint_path):
+    root = tk.Tk()
+    root.withdraw()
+    root.attributes("-topmost", True)
+    selected_dir = filedialog.askdirectory(initialdir=default_checkpoint_path, title='Select a directory with logs')
+    print(f"Selected {selected_dir}")
+    root.destroy()
+    return selected_dir
 
 def start_and_open(start_dir):
     print("Starting tensorboard...")
@@ -11,5 +22,5 @@ def start_and_open(start_dir):
     os.system('python -m tensorboard.main --max_reload_threads 4 --logdir='+ start_dir)
 
 if __name__ == "__main__":
-    model_dir = os.path.dirname(utils.ask_user_for_checkpoint(s.path_to_models))
+    model_dir = ask_user_for_log_dir(s.path_to_models)
     start_and_open(model_dir)
