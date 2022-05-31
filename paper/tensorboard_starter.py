54e3792977be65d379abf50d867b68eb24ed62d0
anion0278 <anion0278@gmail.com>
2022-05-31 08:53:16 +0200

    added GUI for choosing folder for tensorboard

--- paper/tensorboard_starter.py
+++ paper/tensorboard_starter.py
@@ -1,14 +1,15 @@
 import os
 import sys
 import webbrowser
+import common_settings as s
+import model_utils as utils
 import ws_specific_settings as wss
 
-logs_path = wss.storage + ":/models"
-
-def start_and_open():
+def start_and_open(start_dir):
     print("Starting tensorboard...")
     webbrowser.open_new("http://localhost:6006/#scalars")
-    os.system('python -m tensorboard.main --max_reload_threads 4 --logdir='+ logs_path)
+    os.system('python -m tensorboard.main --max_reload_threads 4 --logdir='+ start_dir)
 
 if __name__ == "__main__":
-    start_and_open()
+    model_dir = os.path.dirname(utils.ask_user_for_checkpoint(s.path_to_models))
+    start_and_open(model_dir)
