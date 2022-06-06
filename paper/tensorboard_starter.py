8122e8cc2caf664779c2afd15cdd943cf507acee
anion0278 <anion0278@gmail.com>
2022-06-06 10:36:49 +0200

    GUI for choosing multiple folders for tensorborad

--- paper/tensorboard_starter.py
+++ paper/tensorboard_starter.py
@@ -5,22 +5,26 @@ import common_settings as s
 import model_utils as utils
 import ws_specific_settings as wss
 import tkinter as tk
-from tkinter import filedialog
+import tkfilebrowser
 
-def ask_user_for_log_dir(default_checkpoint_path):
+def ask_user_for_log_dirs(default_checkpoint_path):
     root = tk.Tk()
     root.withdraw()
     root.attributes("-topmost", True)
-    selected_dir = filedialog.askdirectory(initialdir=default_checkpoint_path, title='Select a directory with logs')
-    print(f"Selected {selected_dir}")
+    selected_dirs = tkfilebrowser.askopendirnames(initialdir=default_checkpoint_path, title='Select a directory with logs')
+    print(f"Selected {selected_dirs}")
     root.destroy()
-    return selected_dir
+    return selected_dirs
 
-def start_and_open(start_dir):
+def start_and_open(log_dirs): 
     print("Starting tensorboard...")
     webbrowser.open_new("http://localhost:6006/#scalars")
-    os.system('python -m tensorboard.main --max_reload_threads 4 --logdir='+ start_dir)
+    formatted_dirs = ""
+    for dir in log_dirs: # todo somekind of LINQ
+        name = os.path.basename(dir)
+        formatted_dirs += f"{name}:{dir},"
+    os.system('python -m tensorboard.main --max_reload_threads 4 --logdir_spec='+ formatted_dirs)
 
 if __name__ == "__main__":
-    model_dir = ask_user_for_log_dir(s.path_to_models)
-    start_and_open(model_dir)
+    log_dirs = ask_user_for_log_dirs(s.path_to_models) # we explicitly choose dirs, because it does not work well when root dir is chosen
+    start_and_open(log_dirs)
