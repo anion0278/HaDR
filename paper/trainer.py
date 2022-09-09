3de8d20ac0b0e1c93ed224363c398e3f3dc93ff3
AlesVysocky <AlesVysocky@users.noreply.github.com>
2022-09-09 09:32:09 +0200

    new params for model name

--- paper/trainer.py
+++ paper/trainer.py
@@ -16,14 +16,14 @@ import os, argparse
 import warnings
 warnings.filterwarnings("ignore")  # disables annoying deprecation warnings
 
-default_experiment_tag = "1C"
+default_experiment_tag = "2Rv2"
 # default_arch_name = "fast_mask_rcnn_r101_fpn"
-default_arch_name = "solov2_light_448_r50_fpn"
+# default_arch_name = "solov2_light_448_r50_fpn"
 # default_arch_name = "mask_rcnn_r101_fpn"
-# default_arch_name = "solov2_r101_fpn"
-is_batchnorm_fixed = False
+default_arch_name = "solov2_r101_fpn"
+is_batchnorm_fixed = True
 is_aug_enabled = True
-default_channels = 4
+default_channels = 3
 TEST = False  # if True runs only 100 same images from validation dataset for BOTH TRAIN and VAL
 
 def get_datasets(cfg):
@@ -39,7 +39,7 @@ def manage_aug(cfg, is_aug_enabled):
         cfg.data.train = copy.deepcopy(cfg.data.val)
         # cfg.data.train["pipeline"][3] = train_norm
     return cfg
-
+ 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
     parser.add_argument(
@@ -74,8 +74,10 @@ if __name__ == "__main__":
     print(args)
     storage = wss.storage
     
-    frozen_epochs = 5
+    frozen_epochs = 10
+    frozen_lr = 1e-4 
     unfrozen_epochs = 20
+    unfrozen_lr = 1e-5
     training_dataset = "sim_train_320x256" 
     validation_dataset = "real_merged_l515_640x480"
 
@@ -86,11 +88,13 @@ if __name__ == "__main__":
     train_dataset_path = s.path_to_datasets + training_dataset
     val_dataset_path =  s.path_to_datasets + validation_dataset
     main_channel =  utils.get_main_channel_name(args.channels)
-    timestamp = dt.now().strftime("%a_D%d_M%m_%Hh_%Mm") 
+    timestamp = dt.now().strftime("%a_%d_%b_%Hh_%Mm") 
 
     cfg = utils.get_config(args.arch, args.channels)
 
-    config_id = f"{args.tag}-{args.arch}_{args.channels}ch-{training_dataset}_{dataset_size}-Aug{args.aug}-BS{cfg.data.imgs_per_gpu}-BNfixed{is_batchnorm_fixed}-{frozen_epochs}+{unfrozen_epochs}ep-{timestamp}"
+    config_id = f"{args.tag}-{args.arch}_{args.channels}ch-DS{training_dataset}_{dataset_size}-Aug{args.aug}-BS{cfg.data.imgs_per_gpu}-BNfixed{is_batchnorm_fixed}"\
+                + f"-FrozenEP{frozen_epochs}_LR{frozen_lr}+UnfrozenEP{unfrozen_epochs}_LR{unfrozen_lr}-LRConfig{cfg.lr_config.policy}-{timestamp}"
+
     print("CURRENT CONFIGURATION ID: " + config_id)
     cfg.work_dir = storage + ":/models/" + config_id
     os.makedirs(cfg.work_dir, exist_ok=True)
@@ -107,7 +111,7 @@ if __name__ == "__main__":
 # FULLY FROZEN BACKBONE: https://img1.21food.com/img/cj/2014/10/9/1412794284347212.jpg
 
     cfg.load_from = f"{s.path_to_models}{args.arch}.pth"
-    cfg.optimizer.lr = 1e-3
+    cfg.optimizer.lr = frozen_lr
     cfg.model.backbone.frozen_stages = 4
     cfg.total_epochs = frozen_epochs
     model = build_detector(cfg.model, train_cfg = cfg.train_cfg, test_cfg = cfg.test_cfg)
@@ -121,7 +125,7 @@ if __name__ == "__main__":
 # TODO DRY function
 
     cfg.load_from = latest_checkpoint
-    cfg.optimizer.lr = 1e-4
+    cfg.optimizer.lr = unfrozen_lr
     cfg.model.backbone.frozen_stages = -1
     cfg.total_epochs = unfrozen_epochs
     model = build_detector(cfg.model, train_cfg = cfg.train_cfg, test_cfg = cfg.test_cfg)
