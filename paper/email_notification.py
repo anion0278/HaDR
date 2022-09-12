c819ec5919f18ca727725d9877b2752e390f004a
Grushko Stefan <anion0278@gmail.com>
2022-09-12 10:15:28 +0200

    email notifications (during training) utilizes local outlook app

---
+++ paper/email_notification.py
@@ -0,0 +1,17 @@
+import win32com.client as win32
+import time
+
+def send_email(subject, text, address):
+    outlook = win32.Dispatch('outlook.application')
+    mail = outlook.CreateItem(0)
+    mail.To = address
+    mail.Subject = subject
+    mail.Body = text
+    mail.HTMLBody = f'<h2>{text}</h2>'
+    mail.Send()
+    time.sleep(1)
+    # mapi = outlook.GetNameSpace("MAPI")
+    # inbox = mapi.GetDefaultFolder(4)
+    # inbox.Folders.Session.SendAndReceive(True)
+    # time.sleep(10)
+
