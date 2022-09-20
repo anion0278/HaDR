4533dd748ffc52f1c4c94b9a335aed4f971b09b0
Grushko Stefan <anion0278@gmail.com>
2022-09-20 10:10:19 +0200

    Added info printout during email notification

--- paper/email_notification.py
+++ paper/email_notification.py
@@ -10,6 +10,8 @@ def send_email(subject, text, address):
     mail.HTMLBody = f'<h2>{text}</h2>'
     mail.Send()
     time.sleep(1)
+    print("Sent email to:" + address)
+    print("Email content:" + text)
     # mapi = outlook.GetNameSpace("MAPI")
     # inbox = mapi.GetDefaultFolder(4)
     # inbox.Folders.Session.SendAndReceive(True)
