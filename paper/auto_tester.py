4ba7f50ee879a3516a3c4ad525eadaca1e2d2220
AlesVysocky <AlesVysocky@users.noreply.github.com>
2022-10-07 11:50:28 +0200

    chnaged regex to match any models

--- paper/auto_tester.py
+++ paper/auto_tester.py
@@ -7,7 +7,7 @@ ALL_MODELS_DIR = s.path_to_models
 MODEL_NAME = "final.pth"
 
 for model_dir in os.listdir(ALL_MODELS_DIR):
-    if (re.search("^\d[A-Z]-",model_dir)):
+    if (re.search("^\d[A-Za-z0-9]+-",model_dir)):
         dirname = os.path.join(ALL_MODELS_DIR, model_dir)
         if (os.path.exists(os.path.join(dirname,MODEL_NAME))):
             command = f"python paper/tester.py --checkpoint_path {dirname} --eval segm --out {dirname}\out.pkl"
