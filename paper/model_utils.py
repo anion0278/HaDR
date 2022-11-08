726847b46014072115f1b219681c7d9c24ac75ed
AlesVysocky <AlesVysocky@users.noreply.github.com>
2022-11-08 08:44:33 +0100

    added egohands evaluation

--- paper/model_utils.py
+++ paper/model_utils.py
@@ -11,6 +11,7 @@ import tkinter.messagebox # this is required! see https://stackoverflow.com/a/29
 def ask_user_for_dataset():
     options = {
         "Real cam" : "real_merged_l515_640x480",
+        "Egohands" : "egohands_data",
         "Sim val" : "sim_val_320x256",
         "Sim train" : "sim_train_320x256"}
     val = s.DropDownMenuChoice().show_options(options, "Choose dataset:", default_value=wss.tested_dataset)
