a8b4d2797820ddc3d78950277e9937a1a7acb11b
Vysocky Ales <alivy@seznam.cz>
2022-12-09 10:08:41 +0100

    centroid_tuned for more instances

--- paper/centroid.py
+++ paper/centroid.py
@@ -8,7 +8,9 @@ from ast import literal_eval
 #plt.style.use('_mpl-gallery')
 plt.rcParams.update({'font.size': 18})
 
-dataset_dir_path = "F:/datasets/sim_train_320x256"
+dataset_dir_path = "C:/dataset/sim_val_320x256"
+number_of_instances = 3
+
 centroids_file = os.path.join(dataset_dir_path,"centroid.txt")
 color_dir_path = os.path.join(dataset_dir_path, "color")
 mask_dir_path = os.path.join(dataset_dir_path, "mask2")
@@ -22,10 +24,9 @@ def main():
 
 
 def find_centroids():
+    print("Processing dataset to find centroids")
     centroids = []
-    counts = [0,0,0]
-    #egohands
-    #counts = [0,0,0,0,0]
+    counts = [0]*number_of_instances
 
     name = random.choice(os.listdir(mask_dir_path))
     file = cv2.imread(os.path.join(mask_dir_path,name))
@@ -42,12 +43,9 @@ def find_centroids():
         if(os.path.exists(mask)):
             files.append(cv2.imread(mask,0))
         else:
-            files.append(cv2.imread(os.path.join(mask_dir_path,instance_name + "_i1.png"),0))
-            files.append(cv2.imread(os.path.join(mask_dir_path,instance_name + "_i2.png"),0))
-            #egohands
-            #files.append(cv2.imread(os.path.join(mask_dir_path,instance_name + "_i3.png"),0))
-            #files.append(cv2.imread(os.path.join(mask_dir_path,instance_name + "_i4.png"),0))
-
+            for i in range(1,number_of_instances,1):
+                files.append(cv2.imread(os.path.join(mask_dir_path,instance_name + "_i"+str(i)+".png"),0))
+            
         for file in files:
             M = cv2.moments(file)
             if (M["m00"]!=0):
@@ -59,6 +57,8 @@ def find_centroids():
                 #cv2.imshow("win"+str(len(counts)),file)
         
         counts[instances]+=1
+        if(len(centroids)%10000):
+            print("{} files processed".format(str(len(centroids))))
 
     f = open(centroids_file,'w')
     f.write(str(dims)+"\n")
@@ -70,6 +70,7 @@ def find_centroids():
     return x,y,dims,counts
 
 def read_centroids():
+    print("Reading centroids from file")
     f = open(centroids_file,'r')
     dims_s = f.readline()
     dims = literal_eval(dims_s)
@@ -89,15 +90,12 @@ def show_graph(x,y,dims,counts):
     gs2=gs[1].subgridspec(2,2,height_ratios=[1,8],width_ratios=[8,1],wspace=0.12,hspace=0.06)
     plt.subplots_adjust(left=0.1, bottom=0.1, right=0.95, top=0.9)
 
-
+ 
     bar = fig.add_subplot(gs1[0,0])
-    bar.bar([0,1,2],counts,color = 'blue')
-    #egohands
-    #bar.bar([0,1,2,3,4],counts)
+    bar.bar(list(range(number_of_instances)),counts,color = 'blue')
     bar.grid(visible=None)
-    bar.set_xticks([0,1,2])
-    #egohands
-    #bar.set_xticks([0,1,2,3,4])
+    bar.set_xticks(list(range(number_of_instances)))
+
     bar.set_xlabel("number of instances",labelpad = 10)
     bar.set_ylabel("number of images",labelpad = 10)
 
@@ -105,21 +103,24 @@ def show_graph(x,y,dims,counts):
     scatter.grid(visible=None)
     #scatter.hexbin(x,y,gridsize=(dims[1],dims[0]))
     density = scatter.scatter_density(x,y, color = 'blue',dpi=10)
-    #scatter.scatter(x,y)
     scatter.set_xticks(np.arange(0,dims[1]+32,dims[1]/5))
     scatter.set_yticks(np.arange(0,dims[0]+32,dims[1]/5))
     scatter.set_xlabel("x axis (pixels)",labelpad = 10)
     scatter.set_ylabel("y axis (pixels)",labelpad = 10)
 
+    
+
     y_hist = fig.add_subplot(gs2[1,1], sharey = scatter)
-    y_hist.hist(y,np.arange(0,dims[0]+1,1), orientation="horizontal",color = 'blue')
-    y_hist.set_xticks(np.arange(0,550,500))
+    hy,_,_=y_hist.hist(y,np.arange(0,dims[0]+1,1), orientation="horizontal",color = 'blue')
+    max_hist = np.round(max(hy),-2)+50
+    y_hist.set_xticks([0,max_hist])
     y_hist.grid(visible=None)
     y_hist.tick_params(axis="y", labelleft=False)
 
     x_hist = fig.add_subplot(gs2[0,0], sharex = scatter)
-    x_hist.hist(x,np.arange(0,dims[1]+1,1),color = 'blue')
-    x_hist.set_yticks(np.arange(0,550,500))
+    hy,_,_=x_hist.hist(x,np.arange(0,dims[1]+1,1),color = 'blue')
+    max_hist = np.round(max(hy),-2)+50
+    x_hist.set_yticks([0,max_hist])
     x_hist.tick_params(axis="x", labelbottom=False)
     x_hist.grid(visible=None)
     plt.show()
