779808cbc50e90f1cc39e46da2348abe35c473fb
Grushko Stefan <anion0278@gmail.com>
2023-01-19 15:40:22 +0100

    improved histogram visualization

--- paper/eval_params.py
+++ paper/eval_params.py
@@ -1,4 +1,29 @@
 from pycocotools.cocoeval import Params
+
+def show_hist(data, bins, enable_bar_labels = True):
+        import matplotlib.pyplot as plt
+        counts, edges, bars = plt.hist(data, edgecolor="black", bins=bins)
+        if enable_bar_labels: plt.bar_label(bars)
+        cm = plt.cm.get_cmap('Blues')
+
+        # Plot histogram.
+        bin_centers = 0.5 * (edges[:-1] + edges[1:])
+
+        # scale values to interval [0,1]
+        col = bin_centers - min(bin_centers)
+        col /= max(col)
+
+        range_corr = 0.33
+        col /= (1 / range_corr)
+        col += range_corr
+
+        for c, p in zip(col, bars):
+            plt.setp(p, 'facecolor', cm(c))
+
+        plt.xlabel('Instance area')
+        plt.ylabel('Number of instances')
+        plt.show()
+
 class CustomizedEvalParams(Params):
     def __init__(self, coco_dataset):
         self.smallObjAreaRng, self.mediumObjAreaRng, _ = self.__calc_object_sizes(coco_dataset) # large obj limit is not needed
@@ -21,24 +46,13 @@ class CustomizedEvalParams(Params):
         medium = annotation_areas[len(annotation_areas) * 2 // 3] 
         large = annotation_areas[len(annotation_areas) - 1] 
 
-        # self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
-        # small = 32 ** 2 
-        # medium = 96 ** 2 
-
         print(f"Calculated object sizes from distribution \n Small limit: {small} \n Medium limit: {medium} \n Largest object area: {large}")
 
-        import matplotlib.pyplot as plt
-        plt.hist(annotation_areas, edgecolor="black", bins=50)
-        plt.xlabel('Instance area')
-        plt.ylabel('Number of instances')
-        plt.show()
-
-        counts, edges, bars = plt.hist(annotation_areas, edgecolor="black", bins=[0, small, medium, large])
-        plt.bar_label(bars)
-        for i, color in zip(range(0,3), ["lightblue","skyblue","steelblue"]):
-            bars[i].set_facecolor(color)
-        plt.xlabel('Instance area')
-        plt.ylabel('Number of instances')
-        plt.show()
+        # COCO: self.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 32 ** 2], [32 ** 2, 96 ** 2], [96 ** 2, 1e5 ** 2]]
+        show_hist(annotation_areas,[0, 32 ** 2, 96 ** 2, large])
+        show_hist(annotation_areas,[0, small, medium, large])
+        show_hist(annotation_areas,50, False)
 
         return small, medium, large
+
+    
