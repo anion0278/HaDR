128ff6768e823da44d75812b20cf63acf6685f7b
Grushko Stefan <anion0278@gmail.com>
2023-01-23 10:09:51 +0100

    changed auto train def

--- paper/auto_trainer.py
+++ paper/auto_trainer.py
@@ -1,7 +1,7 @@
 import os
 archs = {
-    #"1": "solov2_light_448_r50_fpn",
-    #"2": "solov2_r101_fpn",
+    "1": "solov2_light_448_r50_fpn",
+    "2": "solov2_r101_fpn",
     "3": "mask_rcnn_r50_fpn",
     "4": "mask_rcnn_r101_fpn",
     }
@@ -10,23 +10,23 @@ ds_3rd_p = "third-party/"
 our_ds = "sim_train_320x256"
 
 data_configs = {
-    #"X": (1, False, our_ds),
-    #"Y": (3, False, our_ds),
-    #"Z": (4, False, our_ds),
+    #"X": (1, True, our_ds),
+    #"Y": (3, True, our_ds),
+    #"Z": (4, True, our_ds),
 
-    #"E": (3, False, ds_3rd_p+"egohands"),
+    #"E": (3, True, ds_3rd_p+"egohands"),
 
-    #"H": (1, False, ds_3rd_p+"handseg"), #chybí mask_r100
+    #"H": (1, True, ds_3rd_p+"handseg"), #chybí mask_r100
 
-    "D": (1, False, ds_3rd_p+"densehands"),
+    "D": (1, True, ds_3rd_p+"densehands"),
 
-    # "O": (1, False, ds_3rd_p+"obman"),
-    # "O": (3, False, ds_3rd_p+"obman"),
-    # "O": (4, False, ds_3rd_p+"obman"),
+    # "O": (1, True, ds_3rd_p+"obman"),
+    # "O": (3, True, ds_3rd_p+"obman"),
+    # "O": (4, True, ds_3rd_p+"obman"),
 
-    # "R": (1, False, ds_3rd_p+"RHD"),
-    # "R": (3, False, ds_3rd_p+"RHD"),
-    # "R": (4, False, ds_3rd_p+"RHD"),
+    # "R": (1, True, ds_3rd_p+"RHD"),
+    # "R": (3, True, ds_3rd_p+"RHD"),
+    # "R": (4, True, ds_3rd_p+"RHD"),
     }
 
 for tag_name, config in data_configs.items():
